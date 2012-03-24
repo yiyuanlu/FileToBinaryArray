6348f93bbe79ee7250c19b4f81f8681027c2f2b7
Luyiyuan <luyiyuan@umeng.com>
2012-03-24 17:10:48 +0800

    Initial Commit

---
+++ FileToBinaryArray/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FileToBinaryArray
+//
+//  Created by umeng on 3/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
