07aa94ea8f3148c17f02550da34167eabb57e74e
Luyiyuan <luyiyuan@umeng.com>
2012-03-27 19:07:50 +0800

    添加自己实现的imageView
    Signed-off-by: Luyiyuan <luyiyuan@umeng.com>

---
+++ FileToBinaryArray/DropImageView.h
@@ -0,0 +1,13 @@
+//
+//  DropImageView.h
+//  FileToBinaryArray
+//
+//  Created by umeng on 3/27/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface DropImageView : NSImageView
+
+@end
---
+++ FileToBinaryArray/DropImageView.m
@@ -0,0 +1,28 @@
+//
+//  DropImageView.m
+//  FileToBinaryArray
+//
+//  Created by umeng on 3/27/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "DropImageView.h"
+
+@implementation DropImageView
+
+- (id)initWithFrame:(NSRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+    // Drawing code here.
+}
+
+@end
